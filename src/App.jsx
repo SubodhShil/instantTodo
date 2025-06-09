1391ac463d160d52e043f47773d878671c661bf8
Subodh Chandra Shil <86912753+SubodhShil@users.noreply.github.com>
2025-06-09 20:02:14 +0600

    fixed input validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import './index.css';
 import Home from './Components/Home';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <ToastContainer />
     </div>
   )
 }
